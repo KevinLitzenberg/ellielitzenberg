e2a38461aaab08a9d36e4ce3aa7c705e155a6711
Kevin Litzenberg <kevin.litzenberg@gmail.com>
2013-04-02 14:06:28 -0700

    Creating the intial repo for www.ellielitzenberg.comZ

---
+++ index.php
@@ -0,0 +1,111 @@
+<html>
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+
+
+<script type="text/javascript">
+
+</script>
+
+<!-------------------------Begin Style Sheets ------------------------->
+<style type="text/css">
+
+/*Redefine html tags */
+html { 
+	background: url(bus19.jpg) no-repeat center center fixed; 
+	-webkit-background-size: cover;
+	-moz-background-size: cover;
+	-o-background-size: cover;
+	background-size: cover;
+}
+
+#divTitle{
+    position: absolute;
+    top: 5px;
+    left: 15px;
+    overflow: hidden;
+    width: 350px;
+    height: 125px;
+	padding: 10px;
+    
+    border: 1px solid #DCDCDC;
+    border-top-left-radius: 15px 22px;
+	border-top-right-radius: 15px 22px;
+	border-bottom-right-radius: 15px 22px;
+	border-bottom-left-radius: 15px 22px;
+	
+	text-indent: 0px;
+	font-family: San-Seirf, Arial;
+	font-size: 25px;
+
+     box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.5);
+    -moz-box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.5);
+    -webkit-box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.5);
+    -o-shadow: 2px 2px 8px rgba(0, 0, 0, 0.5); 	
+
+}
+
+titleMain{font-size:medium;
+      left: 15px;
+      font-family: Ariel;
+      color: #FF8C00; 
+      font-variant: small-caps;
+     }
+     
+titleMain:first-letter{text-transform:uppercase;}  
+
+
+#image_stack img {
+    border: none;
+    text-decoration: none;
+    position: relative;
+    margin-left:0px;
+    width: 697px;
+    height: 1039px;
+    position: absolute;
+    border: 4px solid #FFF;
+    box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.5);
+    -moz-box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.5);
+    -webkit-box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.5);
+	
+	-webkit-transform: rotate(90deg);
+    -moz-transform: rotate(90deg);
+    z-index: 9999;
+   /* -moz-transition: all 0.2s ease;
+    -webkit-transition: all 0.2s ease;
+    -o-transition: all 0.2s ease;
+    transition: all 0.2s ease; */
+}
+
+</style>
+
+<title>EllieLitzenberg.html</title><!-- Captioning Photos -->
+
+</head>
+
+<body>
+	  <div id="divTitle">
+	   <titleMain> 
+	   <bold>Ellie Litzenberg </bold><br>
+	   ellie.litzenberg@gmail.com<br>
+	   (415)685-1550<br>
+	   <a href="../Slideluck/EllieSlideLuck.php">View Slideluck Exhibition</a>
+
+	   <br>
+	   <br>
+	   This site is currenly under development.
+	   
+	   
+	   
+	   </titleMain>
+	  </div>  
+	   
+	 <!--   <div id="image_stack"> --> <!-- <img src="bus19.jpg" alt=""/> --> <!-- </div> -->
+	
+
+</body>
+
+</html>
